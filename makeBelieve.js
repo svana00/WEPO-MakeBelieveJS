4dabe617e7d10c18723bfc23ebf7ce432145e681
svana00 <54863023+svana00@users.noreply.github.com>
2021-02-01 16:17:58 +0000

    fix setup

--- makeBelieve.js
+++ makeBelieve.js
@@ -1,29 +1,67 @@
-var herokuUrl = 'https://serene-island-81305.herokuapp.com';
 
-// GET request
-var basicRequest = new XMLHttpRequest();
-basicRequest.open('GET', herokuUrl);
+(function (globalObj) {
+    // Setup MakeBelieveJS
 
-basicRequest.onreadystatechange = function () {
-    if (basicRequest.readyState === XMLHttpRequest.DONE) {
-        console.log(basicRequest.responseText);
+    // MakeBelieveElement constructor function
+    function MakeBelieveElement(nodes) {
+        // This means this instance of MakeBelieveElement
+        this.nodes = nodes;
     }
-    console.log(basicRequest.readyState);
-};
 
-basicRequest.send();
+    MakeBelieveElement.prototype.getLength = function () {
+        return this.nodes.length;
+    };
 
-// POST request
-var postRequest = new XMLHttpRequest();
-postRequest.open('POST', herokuUrl + '/api/200');
-
-postRequest.setRequestHeader('Content-Type', 'application/json');
-
-postRequest.onreadystatechange = function () {
-    if (postRequest.readyState === XMLHttpRequest.DONE) {
-        console.log(JSON.parse(postRequest.responseText));
+    MakeBelieveElement.prototype.getTagNames = function () {
+        var tagNames = [];
+        for (var i = 0; i < this.nodes.length; i++) {
+            var currentElement = this.nodes[i];
+            tagNames.push(currentElement.tagName.toLowerCase());
+        }
+        return tagNames;
     }
-    console.log(postRequest.readyState);
-};
 
-postRequest.send(JSON.stringify({ a: 1, b: 2, c: 3 }));
+    function query(cssSelector) {
+        return new MakeBelieveElement(document.querySelectorAll(cssSelector));
+    }
+
+    globalObj.__ = query;
+})(window);
+
+var paragraphs = __('p');
+var divs = __('.item');
+console.log(paragraphs.getLength());
+console.log(paragraphs.getTagNames());
+console.log(divs.getLength());
+console.log(divs.getTagNames());
+
+
+// var herokuUrl = 'https://serene-island-81305.herokuapp.com';
+
+// // GET request
+// var basicRequest = new XMLHttpRequest();
+// basicRequest.open('GET', herokuUrl);
+
+// basicRequest.onreadystatechange = function () {
+//     if (basicRequest.readyState === XMLHttpRequest.DONE) {
+//         console.log(basicRequest.responseText);
+//     }
+//     console.log(basicRequest.readyState);
+// };
+
+// basicRequest.send();
+
+// // POST request
+// var postRequest = new XMLHttpRequest();
+// postRequest.open('POST', herokuUrl + '/api/200');
+
+// postRequest.setRequestHeader('Content-Type', 'application/json');
+
+// postRequest.onreadystatechange = function () {
+//     if (postRequest.readyState === XMLHttpRequest.DONE) {
+//         console.log(JSON.parse(postRequest.responseText));
+//     }
+//     console.log(postRequest.readyState);
+// };
+
+// postRequest.send(JSON.stringify({ a: 1, b: 2, c: 3 }));
