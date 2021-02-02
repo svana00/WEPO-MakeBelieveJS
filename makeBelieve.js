6920d19955b477c1d4f4980845f0c99692c4159a
m4gga <margriette123@gmail.com>
2021-02-02 14:25:25 +0000

    comments and starter functions

--- makeBelieve.js
+++ makeBelieve.js
@@ -21,9 +21,45 @@
         return tagNames;
     }
 
+    // 4. find parent method
+    MakeBelieveElement.prototype.parent = function(cssSelector) {
+        return cssSelector;
+    }
+
+    // 5. grandParent method
+    MakeBelieveElement.prototype.grandParent = function(cssSelector) {
+        return 5;
+    }
+
+    // 6. ancestor
+    MakeBelieveElement.prototype.ancestor = function(cssSelector) {
+        return 6;
+    }    
+
+    //  query selector
     function query(cssSelector) {
         return new MakeBelieveElement(document.querySelectorAll(cssSelector));
     }
+
+    // 7.click handler
+
+    // 8. insert text method
+
+    // 9. append HTML method
+
+    // 10. prepend HTML method
+
+    // 11. delete method
+
+    // 12. JQuery ajax method
+
+    // 13. css() method
+
+    // 14. toggleClass() implementation
+
+    // 15. submit handler for forms
+
+    // 16. input handler for input tags
 
     globalObj.__ = query;
 })(window);
@@ -65,3 +101,50 @@ console.log(divs.getTagNames());
 // };
 
 // postRequest.send(JSON.stringify({ a: 1, b: 2, c: 3 }));
+
+
+
+
+
+
+(function (globalObj) {
+    // Setup MakeBelieveJS
+
+    // MakeBelieveElement constructor function
+    function MakeBelieveElement(nodes) {
+        // This means this instance of MakeBelieveElement
+        this.nodes = nodes;
+    }
+
+    MakeBelieveElement.prototype.getLength = function() {
+        return this.nodes.length;
+    };
+
+    MakeBelieveElement.prototype.getTagNames = function() {
+        var tagNames = [];
+        for (var i = 0; i < this.nodes.length; i++) {
+            var currentElement = this.nodes[i];
+            tagNames.push(currentElement.tagName.toLowerCase());
+        }
+        return tagNames;
+    }
+
+
+    function query(cssSelector) {
+        return new MakeBelieveElement(document.querySelectorAll(cssSelector));
+    }
+
+    // find parent
+
+
+    globalObj.__ = query;
+})(window);
+
+var paragraphs = __('p');
+var divs = __('.item');
+var hello = __('p').parent('.item');
+console.log(paragraphs.getLength());
+console.log(paragraphs.getTagNames());
+console.log(divs.getLength());
+console.log(divs.getTagNames());
+console.log(hello);
